f362bea25624aed6c4f28980e903e7bea5894493
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2024-05-12 01:15:01 +0200

    style: fix CI errors

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,10 +2,11 @@ import type { Client } from "@types/xmpp__client";
 import type { ConnectionEvents } from "@types/xmpp__connection";
 
 type AddParameters<
-  TFunction extends (...args: unknown) => unknown,
-  TParameters extends [...args: unknown]
-> = (
-  ...args: [...TParameters, ...Parameters<TFunction>]
-) => ReturnType<TFunction>;
+    TFunction extends (...args: unknown) => unknown,
+    TParameters extends [...args: unknown],
+> = (...args: [...TParameters, ...Parameters<TFunction>]) => ReturnType<TFunction>;
 
-export type XmppEventHandler<TEvent extends keyof ConnectionEvents> = AddParameters<ConnectionEvents[TEvent], [xmpp: Client]>;
+export type XmppEventHandler<TEvent extends keyof ConnectionEvents> = AddParameters<
+    ConnectionEvents[TEvent],
+    [xmpp: Client]
+>;
