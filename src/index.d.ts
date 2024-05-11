e4c689b0b8e36bcae48d4eed377eb8ce041a9b3e
ev3nvy <12035264+ev3nvy@users.noreply.github.com>
2024-05-12 00:25:37 +0200

    feat: implement connection logic

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import type { Client } from "@types/xmpp__client";
+import type { ConnectionEvents } from "@types/xmpp__connection";
+
+type AddParameters<
+  TFunction extends (...args: unknown) => unknown,
+  TParameters extends [...args: unknown]
+> = (
+  ...args: [...TParameters, ...Parameters<TFunction>]
+) => ReturnType<TFunction>;
+
+export type XmppEventHandler<TEvent extends keyof ConnectionEvents> = AddParameters<ConnectionEvents[TEvent], [xmpp: Client]>;
--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,98 @@
-(function() {
-    console.log("hello world");
+import { createInterface } from "node:readline/promises";
+
+import { client, xml } from "@xmpp/client";
+// import debug from "@xmpp/debug";
+
+/**
+ * @type {import('./index').XmppEventHandler<'error'>}
+ */
+function handleError(_xmpp, error) {
+    console.error(error);
+}
+
+/**
+ * @type {import('./index').XmppEventHandler<'offline'>}
+ */
+function handleOffline(_xmpp, _el) {
+    console.log("offline");
+}
+
+/**
+ * @type {import('./index').XmppEventHandler<'stanza'>}
+ */
+async function handleStanza(xmpp, stanza) {
+    if (stanza.is("message")) {
+        await xmpp.send(xml("presence", { type: "unavailable" }));
+        await xmpp.stop();
+    }
+}
+
+/**
+ * @type {import('./index').XmppEventHandler<'online'>}
+ */
+async function handleOnline(xmpp, jid) {
+    console.log("online");
+
+    // makes itself available
+    await xmpp.send(xml("presence"));
+
+    // sends a chat message to itself
+    const message = xml("message", { type: "chat", to: jid }, xml("body", {}, "hello world"));
+    await xmpp.send(message);
+}
+
+/**
+ * @type {import('@xmpp/sasl').CredentialsFactory}
+ */
+async function authenticate(auth, mechanism) {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+    console.debug("authenticate", mechanism);
+
+    // you can hook this up to some function that authenticates instead
+    const credentials = {
+        rsoToken: await rl.question("Please input your rso token: "),
+        pasToken: await rl.question("Please input your pas token: "),
+    };
+    console.debug("authenticating");
+    rl.close();
+
+    try {
+        await auth(credentials);
+        console.debug("authenticated");
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+(async () => {
+    const xmpp = client({
+        // change euw1 to affinity region of your account
+        service: "xmpps://euw1.chat.si.riotgames.com:5223",
+        // change eu1 to domain region of your account
+        domain: "eu1.pvp.net",
+        mech: "X-Riot-RSO-PAS",
+        // if you'd like to just hard-code credentials instead (for testing purposes), you need to
+        // also provide a username field (because otherwise sasl defaults to ANONYMOUS), the
+        // username can be whatever you want (it is internally never used afaik)
+        // credentials: {
+        //     username: "yowaddup",
+        //     rsoToken: "RSO token here",
+        //     pasToken: "PAS token here",
+        // },
+        credentials: authenticate,
+    });
+
+    // uncomment below to enable verbose logging
+    // debug(xmpp, true);
+
+    xmpp.on("error", (...args) => handleError(xmpp, ...args));
+    xmpp.on("offline", (...args) => handleOffline(xmpp, ...args));
+    xmpp.on("stanza", (...args) => handleStanza(xmpp, ...args));
+    xmpp.on("online", (...args) => handleOnline(xmpp, ...args));
+
+    xmpp.start().catch(console.error);
 })();
